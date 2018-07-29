06b39f3716d46a49434e56c8b4509cda532a56e7
unknown <Kysluss@aol.com>
2018-07-28 20:44:03 -0400

    First addition of state in the App component.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './components/Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 }, 
+      { name: 'Manu', age: 29 }, 
+      { name: 'Angelina', age: 27 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('switchNameHandler was  clicked');
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 }, 
+        { name: 'Manu', age: 29 }, 
+        { name: 'Angelina', age: 27 }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" />
-        <Person name="Angelina" age="27">I like tacos and having my butt smacked!</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }

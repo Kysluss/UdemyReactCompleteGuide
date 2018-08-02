159e85c70bfbe5e72ccef4dfc1ff7a2982fe0361
unknown <Kysluss@aol.com>
2018-08-02 19:03:59 -0400

    Updated functional components to class/stateful components
    We are going to experiment with lifecycle methods
    
    In order to use lifecycle methods, we need class based components

--- src/containers/App.js
+++ src/containers/App.js
@@ -58,6 +58,7 @@ class App extends Component {
     return (
       <div className={AppClasses.App}>
         <Cockpit 
+          appTitle={this.props.title} 
           showPersons={this.state.showPersons} 
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler} />
